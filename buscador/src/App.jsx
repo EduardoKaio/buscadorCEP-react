4bfd23ee6d596f156c0736551d9d8fa998048f6e
Kaio Eduardo <kaioeduardo2003@hotmail.com>
2023-09-22 14:31:30 -0300

    tirar complemento

--- buscador/src/App.jsx
+++ buscador/src/App.jsx
@@ -41,12 +41,11 @@ function App() {
 
       {Object.keys(cep).length > 0 && (
         <main className="main">
-        <h2>CEP: {cep.cep}</h2>
-        <span>{cep.logradouro}</span>
-        <span>{cep.complemento}</span>
-        <span>{cep.bairro}</span>
-        <span>{cep.localidade} - {cep.uf}</span>
-      </main>
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
         )}
       
     </div>
